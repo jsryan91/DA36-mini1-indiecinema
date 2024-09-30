008f72ae0f5582d0ca859af5f8ac03a82e2b7712
chaeyeon-h <jackie0912@naver.com>
2024-09-30 16:35:29 +0900

    Feat: admin-entity 수정 완 (아마) !

---
+++ admin/admin_entity.py
@@ -0,0 +1,24 @@
+class Admin:
+    def __init__(self,count):
+        self.margin_rate=0.4
+        self.count=count
+        self.ticket_price=10_000
+
+    def __repr__(self):
+        return f'마진율: {self.margin_rate}, 판매량 : {self.count}, 티켓 값: {self.ticket_price}'
+
+    def get_margin_rate(self):
+        return self.margin_rate
+    def get_count(self):
+        return self.count
+    def get_ticket_price(self):
+        return self.ticket_price
+
+    def set_margin_rate(self,new_rate):
+        self.margin_rate=new_rate
+
+    def set_count(self,new_count):
+        self.count=new_count
+
+    def set_ticket_price(self,new_ticket_price):
+        self.ticket_price=new_ticket_price
