7b4cc52bcbde393f50ea2ce9a168e5e993fef233
jjungah <negmhi29@gmail.com>
2024-10-04 16:03:11 +0900

    Feat: 최종!!!!

--- event/event_repo.py
+++ event/event_repo.py
@@ -1,20 +1,21 @@
-import os
+import datetime, os
 
 class EventRepo:
     def __init__(self):
         self.event_list = []
-        self.set_event_list()
+        self.date = datetime.datetime.now().strftime('%y%m%d')
 
-    def set_event_list(self):
-        # 현재 디렉토리에서 event.txt 파일 불러오기
         current_dir = os.path.dirname(os.path.abspath(__file__))
         event_file_path = os.path.join(current_dir, 'event.txt')
 
         # 파일에서 이벤트 목록 불러오기
-        with open(event_file_path, 'r', encoding='utf-8') as f:
-            for line in f:
-                movie_event = line.strip().split(',') # 영화 제목, 이벤트 내용
-                self.event_list.append(movie_event)
+        with open(event_file_path, 'r',encoding='utf-8') as f:
+            text_line=f.readlines()
+            for line in text_line:
+                line=line.strip().split(",")
+                if int(line[2]) >= int(self.date):
+                    self.event_list.append(line)
+
 
     def get_event_list(self):
         return self.event_list
@@ -25,5 +26,3 @@ class EventRepo:
             if movie_event[0] == title:
                 return movie_event[1] # 이벤트 내용 반환
         return None # 해당 영화에 대한 이벤트 없는 경우
-
-
